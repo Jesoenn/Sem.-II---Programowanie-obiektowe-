7c7af2f3e81c1cc9b3b20f73638f1dc7c2ef10ce
Jesoenn <147878425+Jesoenn@users.noreply.github.com>
2024-05-22 20:42:38 +0200

    Add files via upload

--- Samochod.java
+++ Samochod.java
@@ -2,15 +2,17 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 
 public class Samochod {
-    public int x,y,celX,celY,waga,speed;
+    public int x,y,prevX,prevY;
+    public int celX,celY;
+    public int waga,speed;
     String facing;
     public Image currentImage,up,right,down,left;
     Silnik silnik;
     public Samochod(Silnik silnik){
         this.silnik=silnik;
         //tymczasowo, zrobic losowo, zeby nic na siebie nie nachodzilo - najpierw generowana map
-        x=0;
-        y=0;
+        prevY=y=60;
+        prevX=x=20;
         waga=1500;
         speed=waga/500;
         facing="up";
@@ -30,9 +32,16 @@ public class Samochod {
         if(x<600)
             x+=speed;
         if(y<600)
-            y+=speed;
+          y+=speed;
     }
     public void movement(Graphics2D g2d){
-        g2d.drawImage(down,x,y,silnik.samochodSize,silnik.samochodSize,null);
+        if(prevY>y)
+            g2d.drawImage(up,x,y,silnik.samochodSize,silnik.samochodSize,null);
+        else if(prevY<y)
+            g2d.drawImage(down,x,y,silnik.samochodSize,silnik.samochodSize,null);
+        else if(prevX>x)
+            g2d.drawImage(left,x,y,silnik.samochodSize,silnik.samochodSize,null);
+        else if(prevX<x)
+            g2d.drawImage(right,x,y,silnik.samochodSize,silnik.samochodSize,null);
     }
 }

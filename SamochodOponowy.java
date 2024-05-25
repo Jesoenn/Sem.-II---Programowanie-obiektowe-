6dc2fa070bdc3761b45da526298fde6ee1b0ed4c
Vltima <kacperdachowski4@gmail.com>
2024-05-25 19:12:01 +0200

    Add files via upload

---
+++ SamochodOponowy.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.awt.geom.Ellipse2D;
+
+public class SamochodOponowy extends Samochod{
+
+    SamochodOponowy(Derby derby, int givenX, int givenY)
+    {
+        super(derby, givenX, givenY);
+    }
+    public void BreakEnemyWhell()
+    {
+
+    }
+}
---
+++ SamochodWybuchowy.java
@@ -0,0 +1,19 @@
+package org.example;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.util.Random;
+public class SamochodWybuchowy extends Samochod{
+    double promien_obrazen;
+    public Ellipse2D pole_razenia = new Ellipse2D.Double();
+    SamochodWybuchowy(Derby derby, int givenX, int givenY, double radius)
+    {
+        super(derby, givenX, givenY);
+        this.promien_obrazen = radius;
+        pole_razenia.setFrame(x, y, promien_obrazen, promien_obrazen);
+    }
+    public void Eksplozja()
+    {
+        // to moze do zrobienia jak sie ogarnie strate zycia samochodu
+    }
+}
